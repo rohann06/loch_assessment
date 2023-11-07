0a53e2502d3defdb6390e02cc3b927e95856554b
Rohan <rohanahire651@gmail.com>
2023-11-08 02:44:33 +0530

    user is redirecting to the url

--- app/components/EmailForm/index.tsx
+++ app/components/EmailForm/index.tsx
@@ -1,21 +1,24 @@
 "use client";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const EmailForm = () => {
   const [email, setEmail] = useState("");
+  const router = useRouter();
 
+  //validating the email
   const isEmail = (email: string) =>
     /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
   //Submitting the email for check
   const handleEmailSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
     if (!isEmail(email)) {
       toast.error("Please enter a valid email!");
     } else {
       toast.success("Email is correct....");
+      router.push("https://app.loch.one/welcome");
     }
     setEmail("");
   };
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import EmailForm from "./components/EmailForm";
 export default function Home() {
   return (
     <main className=" md:flex items-center md:h-screen">
+      
+      {/* Detail side */}
       <div className=" md:w-[1334px]  md:h-screen bg-red-400">rohan</div>
 
+      {/* Email side */}
       <div className=" md:h-screen flex justify-center items-center md:w-[640px]">
         <div className=" w-[365px] h-[347px]">
           <div className=" p-2">
@@ -16,11 +19,10 @@ export default function Home() {
               <EmailForm />
             </div>
           </div>
-          
-            <p className=" font-semibold">
-              You'll receive an email with an invite link to join.
-            </p>
-          
+
+          <p className=" font-semibold">
+            You'll receive an email with an invite link to join.
+          </p>
         </div>
       </div>
     </main>
